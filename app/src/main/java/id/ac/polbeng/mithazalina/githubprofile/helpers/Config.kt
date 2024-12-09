008f151a5a8b8124505ecff75cc1782bccc0f599
MithaZalinaRamadhany <mitazalina974@gmail.com>
2024-12-09 14:25:00 +0700

    push

---
+++ app/src/main/java/id/ac/polbeng/mithazalina/githubprofile/helpers/Config.kt
@@ -0,0 +1,15 @@
+package id.ac.polbeng.mithazalina.githubprofile.helpers
+
+import id.ac.polbeng.mithazalina.githubprofile.BuildConfig
+
+
+class Config {
+    companion object {
+        const val SPLASH_SCREEN_DELAY: Long = 3000
+        const val BASE_URL = "https://api.github.com"
+        const val DEFAULT_USER_LOGIN = ""
+        const val PERSONAL_ACCESS_TOKEN = "token ${BuildConfig.ACCESS_TOKEN}"
+
+
+    }
+}
